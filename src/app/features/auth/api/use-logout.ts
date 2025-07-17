d388cfe997d3832720be1265f7e242a469898442
Priyanshu0512 <priyanshuk0595@gmail.com>
2025-07-17 21:19:56 +0530

    Added the invite code for workspaces.

--- src/app/features/auth/api/use-logout.ts
+++ src/app/features/auth/api/use-logout.ts
@@ -24,6 +24,7 @@ export const useLogout = () => {
       router.refresh();
       // window.location.reload();
       queryClient.invalidateQueries({ queryKey: ["current"] });
+      queryClient.invalidateQueries({ queryKey: ["workspaces"] });
     },
     onError: () => {
       toast.error("Failed to log out.");
